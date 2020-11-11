8f5640f8f7ec87ab5c763051da98f1c6be35f17b
regis <regis@mac-mini.local>
2020-11-11 18:56:44 +0100

    init

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index() {
+        $feed = \Feeds::make('https://blog.enguehard.info/feed/', 10);
+        $data = array(
+            'title'     => $feed->get_title(),
+            'permalink' => $feed->get_permalink(),
+            'items'     => $feed->get_items(),
+        );
+
+        return view('welcome', compact('data'));
+    }
+}
